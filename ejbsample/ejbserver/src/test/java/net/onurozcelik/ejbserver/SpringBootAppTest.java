cc3a535e533b7aceeb42a6ddd9a01aaa58f85ed3
onurozcelik <muhendis.onur@gmail.com>
2022-12-02 19:47:21 +0300

    SBM: applied recipe 'initialize-spring-boot-migration'

---
+++ ejbsample/ejbserver/src/test/java/net/onurozcelik/ejbserver/SpringBootAppTest.java
@@ -0,0 +1,13 @@
+package net.onurozcelik.ejbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
